fa689c92ef7ab99976036d1f2c887e56d530369d
Amandeep Kirar <ak4wrapp@gmail.com>
2025-01-27 01:46:33 -0500

    User Info update

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Header.css"; // Import the header styles
+import DownArrow from "./down-arrow.svg"; // Import the SVG down arrow
 
 interface HeaderProps {
   onHamburgerClick: () => void;
@@ -17,6 +18,7 @@ const Header: React.FC<HeaderProps> = ({
     <div className="user-info" onClick={onUserInfoClick}>
       <span className="user-avatar">A</span>
       <span className="user-name">John Doe</span>
+      <img src={DownArrow} alt="Right Arrow" className="submenu-arrow" />
     </div>
   </header>
 );
