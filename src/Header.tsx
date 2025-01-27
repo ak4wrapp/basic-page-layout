44f3515f2d0fa328dc1f7c879cdcdd6be0b25f4e
Amandeep Kirar <ak4wrapp@gmail.com>
2025-01-27 01:47:04 -0500

    Name

--- src/Header.tsx
+++ src/Header.tsx
@@ -16,8 +16,8 @@ const Header: React.FC<HeaderProps> = ({
       &#9776;
     </div>
     <div className="user-info" onClick={onUserInfoClick}>
-      <span className="user-avatar">A</span>
-      <span className="user-name">John Doe</span>
+      <span className="user-avatar">AK</span>
+      <span className="user-name">Amandeep Kirar</span>
       <img src={DownArrow} alt="Right Arrow" className="submenu-arrow" />
     </div>
   </header>
